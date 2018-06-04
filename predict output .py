4f20d6da7d32d518229dd881aa6780738315e16d
Evan Hobbs <evan.hobbs1988@gmail.com>
2018-06-04 16:35:45 -0500

    Add files via upload

---
+++ predict output .py
@@ -0,0 +1,102 @@
+#ANSWER KEY
+""" # answer:5
+    # answer:10
+    # answer:5,10 -correct answer:5
+    # answer:5
+    # answer:5 -correct answer:5,none
+    # answer:print(a(1,2) + a(2,3))-correct answer:print(a(1,2) + a(2,3))                                                          
+      TypeError: unsupported operand type(s) for +: 'NoneType' and 'NoneType'
+    # answer:(2,5) -correct answer:SyntaxError: unexpected EOF while parsing 
+    # answer:10
+    # answer:2,3 -correct answer:3,2
+    # answer:5,10 """
+#QUESTION KEY
+""" def a():
+    return 5
+print(a())     copy
+def a():
+    return 5
+print(a()+a())     
+def a():
+    return 5
+    return 10
+print(a())     
+def a():
+    return 5
+    print(10)
+print(a())     
+def a():
+    print(5)
+x = a()
+print(x)     
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3))   
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5)
+def a():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a)  
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))     
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5))     
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+print(b)
+a()
+print(b)  
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+a()
+print(b)  
+b = 500
+print(b)
+def a():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=a()
+print(b)
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a()
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y) """
